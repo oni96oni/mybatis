0b92baef8afcd747be6d1d4253836874d9ffac1f
oni <rainbow2774@naver.com>
2022-06-24 20:30:05 +0900

    mybatis 예제

--- mybatis01/src/com/conn/db/DBConn.java
+++ mybatis01/src/com/conn/db/DBConn.java
@@ -1,5 +1,6 @@
 package com.conn.db;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Date;
 import java.text.ParseException;
@@ -17,9 +18,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import com.emp.vo.DataVO;
 import com.emp.vo.EmpVO;
 import com.emp.vo.EmpWhereVO;
-import com.emp.vo.JobCountVO;
 
 public class DBConn {
+
 	public static SqlSession getSqlSession() {
 		SqlSession sess = null;
 		
@@ -28,9 +29,9 @@ public class DBConn {
 		
 		try {
 			is = Resources.getResourceAsStream(config);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is,"development");
-			sess = sqlSessionFactory.openSession(false); // 자동커밋을 사용안하겠다!
-		} catch (Exception e) {
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is, "development");
+			sess = sqlSessionFactory.openSession(false);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
@@ -39,39 +40,27 @@ public class DBConn {
 	
 	public static void main(String[] args) {
 		SqlSession session = DBConn.getSqlSession();
-		System.out.println(session.getConfiguration().getEnvironment().getDataSource().toString());
-		int result = session.selectOne("empMapper.empCount");
-		System.out.println(result);
-		System.out.println("mybatis01.DBConn파일체크");
-		System.out.println("result--------------------------------------------------------------------------");
+		int result1 = session.selectOne("empMapper.empCount");
+		System.out.println(result1);
 		
 		List<String> result2 = session.selectList("empMapper.empNames");
 		System.out.println(result2);
-		System.out.println("result2--------------------------------------------------------------------------");
 		
 		List<Object> result3 = session.selectList("empMapper.empDatas");
 		System.out.println(result3);
-		System.out.println("result3--------------------------------------------------------------------------");
 		
-		for(int idx=0; idx< result3.size(); idx++) {
-			Map<String,Object> data = (Map<String, Object>)(result3.get(idx));
-			System.out.println(data.get("EMPLOYEE_ID")+ ", " + data.get("FIRST_NAME"));
+		for(int idx = 0; idx < result3.size(); idx++) {
+			Map<String, Object> data = (Map<String, Object>)(result3.get(idx));
+			System.out.println(data.get("EMPLOYEE_ID") + ", " + data.get("FIRST_NAME"));
 		}
-		System.out.println("data.get(\"EMPLOYEE_ID\")+ \", + data.get(\"FIRST_NAME\")--------------------------------------------------------------------------");
-		
-		Map<Object, Object> result33 = session.selectMap("empMapper.empDatas","EMPLOYEE_ID");
-		System.out.println(result33);
-		System.out.println("result33--------------------------------------------------------------------------");
 		
 		List<EmpVO> result4 = session.selectList("empMapper.empObjects");
 		for(EmpVO data: result4) {
-			System.out.println(data.getEmpId()+ ", "+ data.getFirstName());
+			System.out.println(data.getEmpId() + ", " + data.getFirstName());
 		}
-		System.out.println("result4--------------------------------------------------------------------------");
 		
-		EmpVO result5 = session.selectOne("empMapper.empSelect",100);
-		System.out.println(result5.getEmpId() + ", " + result5.getFirstName());
-		System.out.println("result5--------------------------------------------------------------------------");
+		EmpVO result5 = session.selectOne("empMapper.empSelect", 100);
+		System.out.println(result5.getEmpId()  + ", " + result5.getFirstName());
 		
 		Map<String, Integer> param = new HashMap<String, Integer>();
 		param.put("first", 100);
@@ -80,49 +69,42 @@ public class DBConn {
 		for(EmpVO data: result6) {
 			System.out.println(data.getEmpId() + ", " + data.getFirstName());
 		}
-		System.out.println("result6--------------------------------------------------------------------------");
 		
 		DataVO insertData = new DataVO();
-//		insertData.setId(1);
+		// insertData.setId(5);
 		insertData.setName("test");
-		insertData.setToday(new Date(new java.util.Date().getTime())); // import java.sql.Date;
+		insertData.setToday(new Date(new java.util.Date().getTime()));
 		
 		int result7 = session.insert("empMapper.dataInsert1", insertData);
-		session.commit(); // 직접 커밋할때 사용
+		session.commit();
 		System.out.println(result7 + " 개 행이 추가되었습니다.");
 		
-		
-//		DataVO checkData = session.selectOne("empMapper.inserCheck", insertData.getId());
+//		DataVO checkData = session.selectOne("empMapper.insertCheck", insertData.getId());
 //		if(checkData == null) {
-//			int result7 = session.insert("empMapper.dataInsert1", insertData); // <mapper namespace="empMapper"> + <insert id="dataInsert1" parameterType="dataVo"> = namespace.id
-//			session.commit(); // 직접 커밋할때 사용
+//			int result7 = session.insert("empMapper.dataInsert1", insertData);
+//			session.commit();
 //			System.out.println(result7 + " 개 행이 추가되었습니다.");
 //		} else {
 //			System.out.println("id 컬럼에 " + insertData.getId() + " 에 해당하는 데이터가 이미 존재합니다.");
 //			session.rollback();
 //		}
-//		이 과정을 SEQ_MYBATIS.NEXTVAL을 사용함으로써 생략이 가능하다!!
 		
-		
-//		Map<String, Object> updateData = new HashMap<String, Object>(); // map사용 업데이트
-//		updateData.put("id", 1);
-//		updateData.put("name", "update");
-		
-//		DataVO updateData = new DataVO(); // 객체사용 업데이트
+//		// Map<String, Object> updateData = new HashMap<String, Object>();
+//		// updateData.put("id", 1);
+//		// updateData.put("name", "update");
+//		DataVO updateData = new DataVO();
 //		updateData.setId(1);
 //		updateData.setName("change");
-//		
 //		int result8 = session.update("empMapper.dataUpdate1", updateData);
 //		session.commit();
 //		System.out.println(result8 + " 개 행이 업데이트 되었습니다.");
 //		
 //		int result9 = session.update("empMapper.dataDelete1", 1);
 //		session.commit();
-//		System.out.println(result9 + "개 행이 삭제 되었습니다.");
+//		System.out.println(result9 + " 개 행이 삭제 되었습니다.");
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 		java.util.Date stDate = null, edDate = null;
-		
 		try {
 			stDate = sdf.parse("1990/01/01");
 			edDate = sdf.parse("1999/12/31");
@@ -131,26 +113,64 @@ public class DBConn {
 		}
 		
 		EmpWhereVO dynamicData = new EmpWhereVO();
-		dynamicData.setSalary(10000);
-		dynamicData.setDeptId(80);
-		dynamicData.setStartDate(new Date(stDate.getTime()));
-		dynamicData.setEndDate(new Date(edDate.getTime()));
+		// dynamicData.setSalary(10000);
+		// dynamicData.setDeptId(80);
+		// dynamicData.setStartDate(new Date(stDate.getTime()));
+		// dynamicData.setEndDate(new Date(edDate.getTime()));
 		
 		List<Integer> deptList = new ArrayList<Integer>();
-		deptList.add(80);
-		deptList.add(90);
-		deptList.add(100);
-		
-		dynamicData.setDeptIdList(deptList);
+		deptList.add(80); deptList.add(90); deptList.add(100);
+		// dynamicData.setDeptIdList(deptList);
 		
 		List<EmpVO> result10 = session.selectList("empMapper.dynamicQuery", dynamicData);
-		System.out.println(result10.size() + "개 행의 데이터가 조회 되었습니다.");
+		System.out.println(result10.size() + " 개 행 데이터가 조회 되었습니다.");
 		
-		JobCountVO jobData = new JobCountVO();
-		String[] searchJob = {"AD_PRES", "AD_VP"};
-		jobData.setJobId(searchJob);
+		// Map<String, Integer> paramData = new HashMap<String, Integer>();
+		// paramData.put("deptId", 10);
+		// paramData.put("stDeptId", 10);
+		// paramData.put("edDeptId", 40);
+		Map<String, List<Integer>> paramData = new HashMap<String, List<Integer>>();
+		List<Integer> deptList2 = new ArrayList<Integer>();
+		deptList2.add(10); deptList2.add(20); deptList2.add(40); deptList2.add(70);
+		paramData.put("deptList", deptList2);
 		
-		List<JobCountVO> result11 = session.selectList("empMapper.dynamicQuery2", jobData);
-		System.out.println(result11.size() + "개 행의 데이터가 조회 되었습니다.");
+		List<Map<String, Object>> result11 = session.selectList("empMapper.empOfDeptCount", paramData);
+		
+		for(Map<String, Object> d: result11) {
+			System.out.println(d);
+		}
+		
+		Map<String, Object> paramData2 = new HashMap<String, Object>();
+		paramData2.put("name", "newSeq");
+		paramData2.put("date", new Date(new java.util.Date().getTime()));
+		int id = insertSeqData(paramData2);
+		if(id != -1) {
+			System.out.println("ID 가 " + id + " 인 데이터가 추가 되었습니다.");
+		}
+		
+		DataVO dataVo2 = new DataVO();
+		dataVo2.setName("mybatis_sequence");
+		dataVo2.setToday(new Date(new java.util.Date().getTime()));
+		int result12 = session.insert("empMapper.insertGetSeq", dataVo2);
+		if(result12 ==1) {
+			System.out.println("ID 가 " + dataVo2.getId() + " 인 데이터가 추가 되었습니다.");
+			session.commit();
+		}
+		
 	}
+	
+	public static int insertSeqData(Map<String, Object> param) {
+		SqlSession session = DBConn.getSqlSession();
+		int seq = session.selectOne("empMapper.getSeq");
+		param.put("seq", seq);
+		int result = session.insert("empMapper.insertSeq", param);
+		
+		if(result == 1) {
+			session.commit();
+			return seq;
+		}
+		session.rollback();
+		return -1;
+	}
+
 }
